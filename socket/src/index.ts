11184ebc0503895779ab7c208befc69440e9eaa4
23Aryansharma11 <23aryansharma11@gmail.com>
2025-11-17 10:47:25 +0530

    add backend socket

--- socket/src/index.ts
+++ socket/src/index.ts
@@ -1,15 +1,45 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+// server
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { Server } from "socket.io";
+import { Server as HttpServer } from "http";
+const app = new Hono();
 
-const app = new Hono()
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const server = serve(
+  {
+    fetch: app.fetch,
+    port: 3001,
+  },
+  (info) => {
+    console.log(`Server is running: http://${info.address}:${info.port}`);
+  }
+);
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const ioServer = new Server(server as HttpServer, {
+  serveClient: false,
+});
+ioServer.on("error", (err) => {
+  console.log(err);
+});
+
+ioServer.on("connection", (socket) => {
+  console.log(`${socket.id}: connected`);
+  socket.on("join-room", (data) => {
+    const { userId, roomId } = data;
+    if (!userId || !roomId) {
+      socket.emit("error", { message: "Missing userId or roomId" });
+      return;
+    }
+    socket.join(roomId);
+    console.log(
+      `User ${userId} with socket id ${socket.id} joined room ${roomId}`
+    );
+    socket.emit("joined-room", { roomId });
+  });
+});
+
+// all events join-room joined-room error
--- web/features/room/components/room-client.tsx
+++ web/features/room/components/room-client.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
 import { redirect, useParams, useRouter } from "next/navigation";
 
@@ -7,31 +8,35 @@ import { authClient } from "@/lib/auth-client";
 import { Container } from "@/components/ui/container";
 
 export function RoomClient() {
-	const router = useRouter();
-	const [isAdmin, setIsAdmin] = useState(false);
-	const { roomId } = useParams<{ roomId: string }>();
-	if (!roomId) {
-		router.replace("/rooms");
-	}
 
-	useEffect(() => {
-		const getSession = async () => {
-			const { data: session } = await authClient.getSession();
+	// connect io
+	
 
-			if (!session) {
-				return redirect(`/login/rooms=${roomId}`);
-			}
+  const router = useRouter();
+  const [isAdmin, setIsAdmin] = useState(false);
+  const { roomId } = useParams<{ roomId: string }>();
+  if (!roomId) {
+    router.replace("/rooms");
+  }
 
-			setIsAdmin(session.user.id === roomId);
-		};
-		getSession();
-		return () => {};
-	}, []);
+  useEffect(() => {
+    const getSession = async () => {
+      const { data: session } = await authClient.getSession();
 
-	return (
-		<Container className="h-full flex flex-col px-4  space-y-6 md:space-y-8">
-			<div>{roomId}</div>
-			<div>{isAdmin ? <p>Admin</p> : <p>not admin</p>}</div>
-		</Container>
-	);
+      if (!session) {
+        return redirect(`/login/rooms=${roomId}`);
+      }
+
+      setIsAdmin(session.user.id === roomId);
+    };
+    getSession();
+    return () => {};
+  }, []);
+
+  return (
+    <Container className="h-full flex flex-col px-4  space-y-6 md:space-y-8">
+      <div>{roomId}</div>
+      <div>{isAdmin ? <p>Admin</p> : <p>not admin</p>}</div>
+    </Container>
+  );
 }
